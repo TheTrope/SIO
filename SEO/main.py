a4795c8c633a9f8fd7fb91026993e16f9e351681
franzzy <francois.bosma@gmail.com>
2017-07-31 23:49:04 +0200

    [COMPUTE] having trouble with generators => only one read and eratic order

--- SEO/main.py
+++ SEO/main.py
@@ -36,17 +36,23 @@ def get_kgrams_from_user_inputs():
 
 
     #input shingle size parameter
-    shingle_size = int(input("Shingle size parameter? : "))
+    shingle_size = 3#int(input("Shingle size parameter? : "))
+
+    #DELETEME
+    #urls.append("https://www.amazon.com/ap/signin?openid.assoc_handle=aws&openid.return_to=https%3A%2F%2Fsignin.aws.amazon.com%2Foauth%3Fresponse_type%3Dcode%26client_id%3Darn%253Aaws%253Aiam%253A%253A015428540659%253Auser%252Fhomepage%26redirect_uri%3Dhttps%253A%252F%252Fconsole.aws.amazon.com%252Fconsole%252Fhome%253Fstate%253DhashArgs%252523%2526isauthcode%253Dtrue%26noAuthCookie%3Dtrue&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&action=&disableCorpSignUp=&clientContext=&marketPlaceId=&poolName=&authCookies=&pageId=aws.ssop&siteState=registered%2Cen_US&accountStatusPolicy=P1&sso=&openid.pape.preferred_auth_policies=MultifactorPhysical&openid.pape.max_auth_age=120&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&server=%2Fap%2Fsignin%3Fie%3DUTF8&accountPoolAlias=&forceMobileApp=0&language=en_US&forceMobileLayout=0")
+    #urls.append("https://aws.amazon.com/")
+    urls.append("file:///home/franzzy/Workspace/python/SEO/SEO/one.html")
+    urls.append("file:///home/franzzy/Workspace/python/SEO/SEO/two.html")
 
     #input urls
-    print("Enter some urls, Empty string to stop")
-    url_input_loop_count = 0
-    while True  : 
-        url_input_loop_count += 1
-        url = input("Url " + str(url_input_loop_count) + ": ")
-        if len(url) == 0 :
-            break
-        urls.append(url)
+    #print("Enter some urls, Empty string to stop")
+    #url_input_loop_count = 0
+    #while True  : 
+    #    url_input_loop_count += 1
+    #    url = input("Url " + str(url_input_loop_count) + ": ")
+    #    if len(url) == 0 :
+    #        break
+    #    urls.append(url)
 
     #For each url, get corresponding k-grams
     for url in urls :
@@ -84,9 +90,9 @@ def get_kgrams_from_user_inputs():
 
 
     #display
-    #for pages in urls_kgrams :
-    #    for gram in urls_kgrams[pages] :
-    #        print(gram)
+    """for pages in urls_kgrams :
+        for gram in urls_kgrams[pages] :
+            print(gram)"""
 
 
 
@@ -136,7 +142,7 @@ def make_output(raw_data):
         for x in range(len(data)):
             for y in range(len(data[x])):
                 if (x > 0 and y > 0 and str(data[x][y]) != "X" and float(data[x][y]) >= 0.8):
-                    t.setStyle(TableStyle([('TEXTCOLOR', (x, y), (x, y), colors.red)]))
+                    t.setStyle(TableStyle([('TEXTCOLOR', (y, x), (y, x), colors.red)]))
 
         doc = SimpleDocTemplate("output.pdf", pagesize=A4, rightMargin=30,leftMargin=30, topMargin=30,bottomMargin=18)
         doc.pagesize = landscape(A4)
@@ -166,7 +172,36 @@ def compute_duplicate_rates(urls_kgrams):
     # Inputs: the urls' sketches
     # Output: number between 0 and 1
     def compute_duplicate_rate_between_two_urls(sketch1, sketch2):
-        return 2.1;
+        def compare_shingles(sa, sb):
+            for i in range(len(sa)):
+                if sa[i] != sb[i]:
+                    return False
+            return True
+
+        common_shingle_found = 0
+        shingle_number = 0
+        sketch2_size_counted = False
+        for x in sketch1:
+            shingle_number += 1
+            for y in sketch2:
+                if sketch2_size_counted == False:
+                    shingle_number += 1
+                if compare_shingles(x, y):
+                    common_shingle_found += 1
+                else :
+                    print("==")
+                    print(x)
+                    print(y)
+            sketch2_size_counted = True
+
+
+
+
+
+        print(common_shingle_found)
+        print(shingle_number)
+
+        return 2#common_shingle_found / (shingle_number - common_shingle_found);
 
 
 
@@ -188,6 +223,7 @@ def compute_duplicate_rates(urls_kgrams):
         raw_data.append(line)
 
 
+
     return raw_data;
 
 
@@ -203,11 +239,8 @@ def compute_duplicate_rates(urls_kgrams):
 
 urls_kgrams = get_kgrams_from_user_inputs()
 
+
 raw_data = compute_duplicate_rates(urls_kgrams)
-#raw_data = [["", 0.79, 0.8, 80],
- #               [0, 100, 0, 50], 
-  #              [0.9, 0, 100, 0],
-   #             [80, 50, 0, 100]]
 
 make_output(raw_data)
 
